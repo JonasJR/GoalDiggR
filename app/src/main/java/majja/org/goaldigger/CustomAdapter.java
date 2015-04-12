85811e43479e992909832314d1bc613f8405e5b8
JonasJR <jonas.remgard@gmail.com>
2015-04-12 11:55:38 +0200

    Adds screen for ProjectHandler

---
+++ app/src/main/java/majja/org/goaldigger/CustomAdapter.java
@@ -0,0 +1,33 @@
+package majja.org.goaldigger;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+class CustomAdapter extends ArrayAdapter<String> {
+
+
+    CustomAdapter(Context context, String[] projects) {
+        super(context, R.layout.custom_row, projects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater projectInflater = LayoutInflater.from(getContext());
+        View customView = projectInflater.inflate(R.layout.custom_row, parent, false);
+
+        String singleProjectItem = getItem(position);
+        String[] separated = singleProjectItem.split(",");
+
+        TextView projectName = (TextView) customView.findViewById(R.id.projectName);
+        TextView projectPercentage = (TextView) customView.findViewById(R.id.projectPercentage);
+
+        projectName.setText(separated[0]);
+        projectPercentage.setText(separated[1]);
+
+        return customView;
+    }
+}
--- app/src/main/java/majja/org/goaldigger/ProjectHandlerActivity.java
+++ app/src/main/java/majja/org/goaldigger/ProjectHandlerActivity.java
@@ -1,9 +1,16 @@
 package majja.org.goaldigger;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class ProjectHandlerActivity extends ActionBarActivity {
@@ -12,6 +19,22 @@ public class ProjectHandlerActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_handler);
+
+        String[] projects = {"first,100%", "second,35%", "third,20%", "fourth,99%", "fifth,45%", "sixth,68%"};
+
+        ListAdapter projectAdapter = new CustomAdapter(this, projects);
+        ListView projectListView = (ListView) findViewById(R.id.projectListView);
+        projectListView.setAdapter(projectAdapter);
+
+        projectListView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        String project = String.valueOf(parent.getItemAtPosition(position));
+                        Toast.makeText(ProjectHandlerActivity.this, "Skickar användare till " + project, Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
     }
 
 
