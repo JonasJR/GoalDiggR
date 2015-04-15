312ae0c5c67dbfc4d6cde687549bca3b47992e07
Jerry Pedersen <jerry.ck.pedersen@gmail.com>
2015-04-15 16:26:22 +0200

    Add simple login func. DB.java and Helper.java

---
+++ app/src/main/java/majja/org/goaldigger/DB.java
@@ -0,0 +1,108 @@
+package majja.org.goaldigger;
+
+import android.os.AsyncTask;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Xeronic on 15-04-15.
+ */
+public class DB {
+
+    private HttpClient httpClient = new DefaultHttpClient();
+    private HttpPost request;
+    private JSONObject returnData;
+
+    public JSONObject getReturnData() {
+        return this.returnData;
+    }
+
+    public void login(String email, String password) {
+        JSONObject jsonObj = new JSONObject();
+        try {
+            jsonObj.put("email", email);
+            jsonObj.put("password", password);
+        } catch (JSONException e ) {}
+
+        new Networking("http://goaldigger.herokuapp.com/api/v1/login.json", jsonObj).execute();
+    }
+
+    public void createUser(String username, String email, String password) {
+        JSONObject jsonObj = new JSONObject();
+        try {
+            jsonObj.put("username", username);
+            jsonObj.put("email", email);
+            jsonObj.put("password", password);
+        } catch (JSONException e ) {}
+
+        new Networking("http://goaldigger.herokuapp.com/api/v1/create.json", jsonObj).execute();
+    }
+
+    public void getJSON(String url, JSONObject jsonObj) {
+        request = new HttpPost(url);
+
+        BufferedReader bufferedReader = null;
+
+        StringBuffer stringBuffer = new StringBuffer("");
+
+        try {
+
+            //UrlEncodedFormEntity entity = new UrlEncodedFormEntity(postParameters);
+            StringEntity entity = new StringEntity(jsonObj.toString(), HTTP.UTF_8);
+            entity.setContentType("application/json");
+            request.setEntity(entity);
+
+            HttpResponse response = httpClient.execute(request);
+            bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+            String line = "";
+            String LineSeparator = System.getProperty("line.separator");
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuffer.append(line + LineSeparator);
+            }
+            bufferedReader.close();
+
+        } catch (IOException e) {
+            Helper._("IOException: " + e.getMessage());
+        } catch (Exception e) {
+            Helper._("Exception: " + e.getMessage());
+        }
+
+        JSONObject returnJSON = null;
+        try {
+            returnJSON = new JSONObject(stringBuffer.toString());
+
+        } catch (JSONException e) {}
+
+        Helper._("Return mess: " + stringBuffer.toString());
+
+        this.returnData = returnJSON;
+    }
+
+    public class Networking extends AsyncTask {
+
+        private String url;
+        private JSONObject obj;
+
+        public Networking(String url, JSONObject obj) {
+            this.url = url;
+            this.obj = obj;
+        }
+        @Override
+        protected Object doInBackground(Object[] params) {
+            getJSON(url, obj);
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/majja/org/goaldigger/Helper.java
@@ -0,0 +1,38 @@
+package majja.org.goaldigger;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by Xeronic on 15-04-15.
+ */
+public class Helper {
+
+    private static final String DEBUG_TAG = "Goaldigger";
+
+    /**
+     * Method to show a toast
+     * @param message
+     * @param context
+     */
+    public static void toast(final String message, final Context context) {
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /**
+     * Method used for debugging messages.
+     * @param message
+     */
+    public static void _(String message) {
+        Log.d(DEBUG_TAG, message);
+    }
+}
