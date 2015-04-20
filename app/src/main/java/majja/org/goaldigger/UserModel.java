8de5490ce4e8a399771a0496e4bec72f39163e80
Jerry Pedersen <jerry.ck.pedersen@gmail.com>
2015-04-20 10:52:53 +0200

    Add createUser to DB and UserModel

--- app/src/main/java/majja/org/goaldigger/UserModel.java
+++ app/src/main/java/majja/org/goaldigger/UserModel.java
@@ -1,14 +1,21 @@
 package majja.org.goaldigger;
 
+import org.json.JSONObject;
+
 /**
  * Created by xeronic on 2015-04-16.
  */
 public class UserModel {
     private String email, password, username;
 
-    public UserModel(String email, String password) {
+    public UserModel(String username, String email, String password) {
         this.email = email;
         this.password = password;
+        this.username = username;
+    }
+
+    public String username() {
+        return this.username;
     }
 
     public String email() {
@@ -20,6 +27,23 @@ public class UserModel {
     }
 
     public String toString() {
-        return "Email: " + email + " Password: " + password;
+        return "Username: " + username + "Email: " + email + " Password: " + password;
+    }
+
+    public static UserModel createUser(String username, String email, String password, String passwordConfirmation) {
+        DB db = DB.getInstance();
+        db.createUser(username, email, password, passwordConfirmation);
+        String returnData = db.getReturnData();
+        JSONObject jo = null;
+        UserModel user = null;
+
+        try {
+            jo = new JSONObject(returnData);
+            user = new UserModel(jo.getString("username"), jo.getString("email"), password);
+        } catch (Exception e) {
+            Helper._("Couldn't create user: " + e.getMessage());
+        }
+
+        return user;
     }
 }
