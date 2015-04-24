ba7debf6e9636f597a9fb85af1aaaaaa903d081e
Jerry Pedersen <jerry.ck.pedersen@gmail.com>
2015-04-25 01:10:45 +0200

    Calc height of lists

--- app/src/main/java/majja/org/goaldigger/ProjectActivity.java
+++ app/src/main/java/majja/org/goaldigger/ProjectActivity.java
@@ -5,43 +5,41 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 
 public class ProjectActivity extends ActionBarActivity {
 
-    Context context;
-    Project project;
+    private Project project;
     private ListView projectListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project);
+
         projectListView = (ListView) findViewById(R.id.projectListView);
-        this.context = this.getBaseContext();
-
         project = (Project)getIntent().getExtras().getSerializable("project");
-        Helper.pelle(project.toString());
 
-        addMilestones();
-        addItems();
-
-    }
-
-    private void addMilestones() {
-        ListAdapter milestoneAdapter = new CustomMilestoneAdapter(this, project.getMilestones());
-        //ListView projectListView = (ListView) findViewById(R.id.projectListView);
+        ListAdapter milestoneAdapter = new MilestoneAdapter(this, project.getMilestones());
+        projectListView = (ListView) findViewById(R.id.projectListView);
         projectListView.setAdapter(milestoneAdapter);
-    }
 
-    private void addItems() {
-        ListAdapter itemAdapter = new CustomItemAdapter(this, project.getItems());
-        //ListView projectListView = (ListView) findViewById(R.id.projectListView);
-        projectListView.setAdapter(itemAdapter);
+        projectListView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        String food = String.valueOf(parent.getItemAtPosition(position));
+                        Toast.makeText(ProjectActivity.this,food, Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
