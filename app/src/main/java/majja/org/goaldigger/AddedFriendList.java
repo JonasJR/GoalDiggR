49acbd4da79a96d26a7957554532bd97762f6157
Marten <persson_dunk@msn.com>
2015-05-11 14:11:56 +0200

    clean

--- app/src/main/java/majja/org/goaldigger/AddedFriendList.java
+++ app/src/main/java/majja/org/goaldigger/AddedFriendList.java
@@ -43,11 +43,8 @@ public class AddedFriendList extends ActionBarActivity {
                         shareFriends += ":" + friend.getId();
                     }
                 }
-                User.getInstance().share(shareFriends, project.id());
-
                 if (shareFriends != null) {
                     User.getInstance().share(shareFriends, project.id());
-                    Helper.pelle(project.id() + "Kolla id");
                 }
 
                 Intent intent = new Intent(AddedFriendList.this, ProjectActivity.class);
