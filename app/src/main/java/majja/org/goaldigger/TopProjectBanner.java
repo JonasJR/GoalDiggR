572ef16c54f3d3f403993fcc1f2a574b183d204e
JonasJR <jonas.remgard@gmail.com>
2015-05-14 15:49:46 +0200

    logout added

--- app/src/main/java/majja/org/goaldigger/TopProjectBanner.java
+++ app/src/main/java/majja/org/goaldigger/TopProjectBanner.java
@@ -3,6 +3,9 @@ package majja.org.goaldigger;
 /**
  * Created by Anton on 2015-04-12.
  */
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -13,7 +16,7 @@ import android.widget.Button;
 
 public class TopProjectBanner extends Fragment{
     private Button bigLabel;
-    private Button smallButton;
+    private Button logoutButton;
     private DB db= DB.getInstance();
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -21,10 +24,31 @@ public class TopProjectBanner extends Fragment{
         bigLabel = (Button)view.findViewById(R.id.mainFragmentButton);
         bigLabel.setText("apa");
 
-        smallButton= (Button)view.findViewById(R.id.menuButton);
-        smallButton.setOnClickListener(new View.OnClickListener(){
+        logoutButton= (Button)view.findViewById(R.id.menuButton);
+        logoutButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
 
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+
+                alertDialogBuilder.setTitle("Do you want to log-out?");
+                alertDialogBuilder.setCancelable(false).setPositiveButton("YES",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Helper.toast("You logged out", getActivity());
+                                SaveSharedPreference.logout(getActivity());
+                                Intent intent = new Intent(getActivity(), MainActivity.class);
+                                startActivity(intent);
+                                return;
+                            }
+                        }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                        return;
+                    }
+                });
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
             }
             });
         return view;
