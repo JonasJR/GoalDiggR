fdff44d6e52acd761ebc695ea0d48b72ab3ec327
JonasJR <jonas.remgard@gmail.com>
2015-05-14 12:22:50 +0200

    tog bort loopen så man får notification dirr

--- app/src/main/java/majja/org/goaldigger/GcmIntentService.java
+++ app/src/main/java/majja/org/goaldigger/GcmIntentService.java
@@ -52,15 +52,8 @@ public class GcmIntentService extends IntentService {
                 // If it's a regular GCM message, do some work.
             } else if (GoogleCloudMessaging.
                     MESSAGE_TYPE_MESSAGE.equals(messageType)) {
-                // This loop represents the service doing some work.
-                for (int i = 0; i < 5; i++) {
-                    Helper.pelle("Working... " + (i + 1)
-                            + "/5 @ " + SystemClock.elapsedRealtime());
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                    }
-                }
+
+
                 Helper.pelle("Completed work @ " + SystemClock.elapsedRealtime());
                 // Post notification of received message.
                 sendNotification("Received: " + extras.toString());
