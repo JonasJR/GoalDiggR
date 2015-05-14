54a98c447440f3d616f795a8837252185acde559
Anton Nilsson <anton.h.nilsson@gmail.com>
2015-05-14 15:34:55 +0200

    fragments

--- app/src/main/java/majja/org/goaldigger/TopProjectBanner.java
+++ app/src/main/java/majja/org/goaldigger/TopProjectBanner.java
@@ -9,12 +9,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class TopProjectBanner extends Fragment{
+    private Button bigLabel;
+    private Button smallButton;
+    private DB db= DB.getInstance();
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.top_projekt_fragment, container, false);
+        bigLabel = (Button)view.findViewById(R.id.mainFragmentButton);
+        bigLabel.setText("apa");
+
+        smallButton= (Button)view.findViewById(R.id.menuButton);
+        smallButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+
+            }
+            });
         return view;
-        //Får inte riktigt rätt på hur man får den att ligga rätt?
     }
 }
---
+++ app/src/main/java/majja/org/goaldigger/TopProjectBannerPlain.java
@@ -0,0 +1,20 @@
+package majja.org.goaldigger;
+
+/**
+ * Created by Anton on 2015-04-12.
+ */
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TopProjectBannerPlain extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.top_projekt_fragment_plain, container, false);
+        return view;
+        //Får inte riktigt rätt på hur man får den att ligga rätt?
+    }
+}
