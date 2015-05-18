63dbb11f552108f807aa3b2c5be37c965c034893
Anton Nilsson <anton.h.nilsson@gmail.com>
2015-05-18 12:02:11 +0200

    owner label in projectlist

--- app/src/main/java/majja/org/goaldigger/ProjectAdapter.java
+++ app/src/main/java/majja/org/goaldigger/ProjectAdapter.java
@@ -1,6 +1,8 @@
 package majja.org.goaldigger;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,14 +14,20 @@ import android.widget.TextView;
  * Created by Goaldigger on 2015-04-16.
  */
 class ProjectAdapter extends ArrayAdapter<Project> {
+    private Project project;
+    private User user;
 
 
     ProjectAdapter(Context context, Project[] projects) {
         super(context, R.layout.custom_project_row, projects);
+        user = User.getInstance();
+
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        user = User.getInstance();
+
         LayoutInflater projectInflater = LayoutInflater.from(getContext());
         View customView = projectInflater.inflate(R.layout.custom_project_row, parent, false);
 
@@ -29,6 +37,9 @@ class ProjectAdapter extends ArrayAdapter<Project> {
 
         projectName.setText(getItem(position).name());
         projectPercentage.setText(getItem(position).percent() + "%");
+        if(!getItem(position).owner().equals(user.email())){
+            projectPercentage.setText(getItem(position).percent() + "%  " + getItem(position).owner());
+        }
         progress.setProgress(getItem(position).percent());
         /*if(getItem(position).percent() == 100){
             progress.getProgressDrawable().setColorFilter(Color.GREEN, PorterDuff.Mode.SRC_IN);
